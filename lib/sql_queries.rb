03718fb8de3e874dddc78b0f4f59141868cd7f79
Warren Yao <warrenyca905@gmail.com>
2019-07-24 01:23:36 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,13 +1,13 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT name, age FROM bears WHERE gender = F"
+  "SELECT name, age FROM bears WHERE gender = F;"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "SELECT name ORDER BY name"
+  "SELECT name FROM bears ORDER BY name;"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "Write your SQL query here"
+  "SELECT name, age FROM bears WHERE alive = "
 end
 
 def selects_oldest_bear_and_returns_name_and_age
